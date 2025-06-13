cc316b232f9d60c165655b300874bb421ec8b54e
kimyoonjoo <kimyoonjoo20@gmail.com>
2025-06-13 15:09:35 +0900

    경매이력

--- src/main/react/src/bidHistory/App.jsx
+++ src/main/react/src/bidHistory/App.jsx
@@ -3,13 +3,13 @@ import './bidHistory.css';
 export default function App() {
 
     return (
-        <div className="bidHistory-container" onClick={()=>{window.location.href='./auctionDetail.do'}}>
+        <div className="bidHistory-container">
             <h3 className="bidHistory-title">경매 이력</h3>
             <div
                 className="history-card"
                 style={{ cursor: 'pointer' }} // 클릭 가능한 UI 개선
             >
-                <div className="card-header">
+                <div className="card-header" onClick={()=>{window.location.href='./auctionDetail.do'}}>
                     <div className="left-section">
                         <span className="round-number">1회차</span>
                         <div className="bid-title">경매 제목: 오늘 엄청난 물건 나옵니다!!</div>
@@ -24,6 +24,8 @@ export default function App() {
                     <span>생활용품</span>
                 </div>
             </div>
+
+            <img src="/img/home.png" alt="마이페이지" className="myPage-btn" onClick={ ()=>{window.location.href="./myPage.do"}}/>
         </div>
     );
 }
